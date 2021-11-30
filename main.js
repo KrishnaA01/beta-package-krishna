e9c9cfe58f08d9548b3cc9c4b28cd11b5229074f
KrishnaA01 <mahato77@hotmail.com>
2021-11-30 13:19:28 +0100

    tesing packages

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+//fix bug (path release)
+function sum(a,b){
+    console.log(`sum of two numbers are : ${a+b}`);
+}
+
+//add new features(minor release)
+function multiply(a,b){
+    console.log(`product of two numbers are : ${a*b}`);
+}
+
+
+
+module.exports= { sum, multiply }
